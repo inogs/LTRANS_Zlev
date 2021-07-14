91b87ccd9d308e9aacaf0b68226430224fbb4ede
CeliaLaurent <claurent@inogs.it>
2021-07-14 11:49:43 +0200

    Add VAR_IDs.h

---
+++ LTRANS_src/VAR_IDs.h
@@ -0,0 +1,60 @@
+#define VAR_ID_GrainSize 1
+
+#define VAR_ID_depth 5
+
+#define VAR_ID_angle 10
+
+#define VAR_ID_zeta 15
+#define VAR_ID_zetab 16
+#define VAR_ID_zetac 17
+#define VAR_ID_zetaf 18
+
+#define VAR_ID_salt 20
+#define VAR_ID_saltb 21
+#define VAR_ID_saltc 22
+#define VAR_ID_saltf 23
+
+#define VAR_ID_temp 25
+#define VAR_ID_tempb 26
+#define VAR_ID_tempc 27
+#define VAR_ID_tempf 28
+
+#define VAR_ID_den 30
+#define VAR_ID_denb 31
+#define VAR_ID_denc 32
+#define VAR_ID_denf 33
+
+#define VAR_ID_uvel 35
+#define VAR_ID_uvelb 36
+#define VAR_ID_uvelc 37
+#define VAR_ID_uvelf 38
+
+#define VAR_ID_vvel 40
+#define VAR_ID_vvelb 41
+#define VAR_ID_vvelc 42
+#define VAR_ID_vvelf 43
+
+#define VAR_ID_wvel 45
+#define VAR_ID_wvelb 46
+#define VAR_ID_wvelc 47
+#define VAR_ID_wvelf 48
+
+#define VAR_ID_kh 50
+#define VAR_ID_khb 51
+#define VAR_ID_khc 52
+#define VAR_ID_khf 53
+
+#define VAR_ID_uwind 55
+#define VAR_ID_uwindb 56
+#define VAR_ID_uwindc 57
+#define VAR_ID_uwindf 58
+
+#define VAR_ID_vwind 60
+#define VAR_ID_vwindb 61
+#define VAR_ID_vwindc 62
+#define VAR_ID_vwindf 63
+
+#define VAR_ID_iwind 65
+#define VAR_ID_iwindb 66
+#define VAR_ID_iwindc 67
+#define VAR_ID_iwindf 68
